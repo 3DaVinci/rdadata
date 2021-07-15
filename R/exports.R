f1353f8ee69d9b8a850a305400209706a8145c08
Stanislav Masuta <masuta92@gmail.com>
2021-07-15 19:24:15 +0300

    ADD MVP functions
    added get_organisation function

--- R/exports.R
+++ R/exports.R
@@ -29,3 +29,89 @@ save_dadata_tokens <- function(api_token = NULL,
   Sys.setenv("RDADATA" = env_name)
 
 }
+
+get_organisation <- function(query,
+                             kpp = NULL,
+                             branch_type = NULL,
+                             type = NULL,
+                             count = 300, tidy = TRUE, ...){
+
+  query <- as.character(query)
+  api_token <- get_dadata_tokens()[,"API_TOKEN"]
+
+  if(is.null(api_token)){
+    stop(call. = FALSE, "Please set api_token in save_dadata_tokens()")
+  }
+
+
+  # Check inputs
+
+  if(!is.null(branch_type)){
+    if(!branch_type %in% c("MAIN", "BRANCH")){
+      stop(call. = FALSE, "branch_type should be MAIN or BRANCH")
+    }
+  }
+
+  if(!is.null(type)){
+    if(!type %in% c("LEGAL", "INDIVIDUAL")){
+      stop(call. = FALSE, "branch_type should be LEGAL or INDIVIDUAL")
+    }
+  }
+
+  if(grepl("[A-z]|[А-я]", query)){
+    stop(call. = FALSE, "query should be INN or OGRN number")
+  }
+
+  if(!is.numeric(count)){
+    stop(call. = FALSE, "count should be numeric")
+  }
+
+  if(count > 300){
+    stop(call. = FALSE, "maximum inrow count it's 300")
+  }
+
+  if(!is.logical(tidy)){
+    stop(call. = FALSE, "tidy should be TRUE or FALSE")
+  }
+
+  # Request to server
+  organisation_list <- list()
+  response <- try(httr::POST(
+    url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party",
+    httr::content_type_json(),
+    httr::accept_json(),
+    httr::user_agent("github.com/3davinci"),
+    httr::add_headers(Authorization = paste0("Token ", api_token)),
+    body = list(query = query,
+                count = count,
+                kpp = kpp,
+                branch_type = branch_type,
+                type = type),
+    encode = "json"),
+    silent = TRUE)
+
+  if(class(response)  == "try-error"){
+    stop(call. = FALSE, "Can't request server. Check your internet connection")
+  }
+
+  raw_content <-  httr::content(response)
+
+  if(tidy){
+    tmp_table <- tibble::tibble()
+    for (i in seq(length(raw_content$suggestions))) {
+      tmp_table <- tmp_table %>%
+        dplyr::bind_rows(tidyjson::spread_all(raw_content$suggestions[i]))
+    }
+
+    # convert all dates from numeric do date time
+    tmp_table[names(tmp_table) %>% stringr::str_detect("date")]  <- tmp_table[names(tmp_table) %>% stringr::str_detect("date")] %>% dplyr::mutate_all(~as.Date(as.POSIXct(./1000, origin = "1970-01-01")))
+
+    output_table <- tmp_table %>%
+      dplyr::select(-document.id, -..JSON)
+
+  } else {
+    output_table <- raw_content$suggestions
+  }
+
+  return(output_table)
+}
